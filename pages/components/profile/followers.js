089c55333f101d1b4d0326ae53b34cb8d4a04990
Parshvi <parshvisri@gmail.com>
2022-07-11 17:33:40 +0530

    follower show page

---
+++ pages/components/profile/followers.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { getAccount, connectContract } from "../../../ether-utils";
+import Card from '../../shared/Card';
+const Followers =(props)=>{
+    const[followers, setFollowers] = useState([]);
+    useEffect(()=>{
+        if (props.handle) {
+          getFollower();
+        }
+    },[])
+    const getFollower =async() =>{
+        await getAccount();
+        const elpis = await connectContract();
+        const follwerMetaData = await axios.get(props.handle);
+        console.log(follwerMetaData.data.followers.length);
+        let _followers=[]
+        for (let i = 0; i < follwerMetaData.data.followers.length; i++) {
+          let _follower = await elpis.searchProfile(
+            follwerMetaData.data.followers[i]
+          );
+          console.log(_follower);
+          let dummyFollower = {
+            name: _follower.name,
+            handle: _follower.handle,
+            bio: _follower.bio,
+            profileImageUrl:_follower.profileImageUrl,
+            coverImageUrl:_follower.coverImageUrl,
+            follwerCount: parseInt(_follower.followerCount._hex),
+            followingCount: parseInt(_follower.followingCount._hex),
+          };
+         
+          _followers.push(dummyFollower);
+        }
+        setFollowers(_followers);
+        
+
+    }
+    return (
+      <div>
+        Followers
+        {followers.length == 0 && (
+          <div> You don't have any follower yet !!</div>
+        )}
+        {followers.length > 0 &&
+          followers.map((follower) => {
+            console.log(follower);
+          return(<Card key={follower.handle} profile={follower}/>)
+          }
+          )}
+      </div>
+    );
+}
+export default Followers;
