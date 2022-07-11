dba017f767193d820d3ab78ff06942464b2b8e78
Parshvi <parshvisri@gmail.com>
2022-07-11 17:49:26 +0530

    modifying search bar

--- pages/components/profile/profile.js
+++ pages/components/profile/profile.js
@@ -106,7 +106,6 @@ const Profile = (props) => {
   return (
     <div className="w-full h-full">
       <label
-        for="default-search"
         className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
       >
         Search
@@ -120,12 +119,6 @@ const Profile = (props) => {
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            ></path>
           </svg>
         </div>
         <input
