6a00458ebe4e9158f0f8e46132fc9b43b7436488
Parshvi <parshvisri@gmail.com>
2022-07-11 17:32:50 +0530

    Adding haeader

---
+++ pages/components/header.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+ const Header =()=>{
+    return (
+      <Head>
+        <title>Elpis</title>
+        <meta name="Decentralised Twitter App" content="Created By Parshvi" />
+        <link rel="icon" href="/Logo.png" />
+      </Head>
+    );
+ }
+ export default Header
