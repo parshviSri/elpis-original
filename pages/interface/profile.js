7b6a268f0a6149da5692255f0b9be535134fb2bf
Parshvi <parshvisri@gmail.com>
2022-07-11 17:35:01 +0530

    profile interface is added

---
+++ pages/interface/profile.js
@@ -0,0 +1,14 @@
+export const profileInterface={
+        name:'',
+        handle:'',
+         bio:'',
+         profileImageUrl:'',
+         coverImageUrl:'',
+         followerCount:0,
+         followingCount:0,
+         postCount:0,
+         followerMetaData:'',
+         followingMetaData:'',
+         postMetaData:'',
+         profileExist:''
+}
