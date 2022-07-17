6a70ff7387cc297fed5cff2c044993e7395c2003
Parshvi <parshvisri@gmail.com>
2022-07-17 10:40:46 +0530

    Updating Contract address

--- ether-utils.js
+++ ether-utils.js
@@ -1,6 +1,6 @@
 import { ethers } from "ethers";
 import Elpis from './artifacts/contracts/Elpis.sol/Elpis.json';
-const contractAddress = "0xf435d9fd013e28e85724edcf2b3ec671967babbd";
+const contractAddress = "0x2F05800eA8152e5548b288588eB9EeD1c2947DE6";
 export const getAccount = async () => {
   await window.ethereum.request({ method: "eth_requestAccounts" });
   const accounts = await window.ethereum.request({ method: "eth_accounts" });
