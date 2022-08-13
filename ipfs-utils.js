671e6626da7b2588957a37f7e0b43ca92de7f302
parshviSri <parshvisri@gmail.com>
2022-08-13 10:36:49 +0530

    infura bug
    Signed-off-by: parshviSri <parshvisri@gmail.com>

--- ipfs-utils.js
+++ ipfs-utils.js
@@ -1,12 +1,26 @@
 import { create as ipfsHttpClient } from "ipfs-http-client";
-const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
+ const projectId = "2DHokEdxuooxCjqRGClU6INqosy";
+ const projectSecret = "ca0a080aa589add4b86cef7f5bcc9736";
+ const auth =
+   "Basic " + Buffer.from(projectId + ":" + projectSecret).toString("base64");
+
+ const client = ipfsHttpClient({
+   host: "ipfs.infura.io",
+   port: 5001,
+   protocol: "https",
+   headers: {
+     authorization: auth,
+   },
+ });
 export const addFile = async (_file) => {
   const file = _file;
   try {
     const added = await client.add(file, {
       progress: (prog) => console.log(`received: ${prog}`),
     });
-    const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+    console.log(added);
+    const url = `https://elpis.infura-ipfs.io/ipfs/${added.path}`;
+    console.log(url);
     return url;
   } catch (error) {
     console.log("Error uploading file: ", error);
